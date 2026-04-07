748b62e9ca36fba8254a6076b3b607897e4cea43
4r7n <atj02005@gmail.com>
2026-04-07 18:34:36 +0100

    5

---
+++ p5.py
@@ -0,0 +1,9 @@
+#ok for this one i did some long math at the gym
+#im not sure if my reasoning is logically sound, but it gave the solution
+#lets look at 2520, the number that is evenly divisible from digits 1 through 10
+#2520 is 5*7*8*9
+#now what i concluded after going on a few tangents is that the sum smallest number is creater by taking the prime factors ^ k
+#where f^k (k>=1) is less than or equal to the number so for 1-20 we can conclude
+#19*17*13*7*5*3^2*2^4
+
+print(19*17*16*13*11*9*7*5)
