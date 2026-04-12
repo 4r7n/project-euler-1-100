48840228e12bfcbb355ac68fd9719d27065c3ebd
4r7n <atj02005@gmail.com>
2026-04-12 01:12:18 +0100

    35

--- p35.py
+++ p35.py
@@ -1,4 +1,4 @@
-def eratSieve(n):
+def eratos(n):
     sieve = bytearray([1]) * (n+1)  #p27 for more context
     sieve[0:2] = b'\x00\x00'
 
@@ -6,4 +6,18 @@ def eratSieve(n):
         if sieve[i]:
             sieve[i*i:n+1:i] = b'\x00' * len(sieve[i*i:n+1:i])
 
-    return [i for i in range(n+1) if sieve[i]]
+    return set([i for i in range(n+1) if sieve[i]])
+
+
+primes = eratos(10**6)
+circular = 0
+
+
+for i in primes:
+    k = str(i)
+    if not sum([int(not(int(k[j:] + k[:j]) in primes)) for j in range(len(k))]):
+        circular+=1
+
+
+
+print(circular)
