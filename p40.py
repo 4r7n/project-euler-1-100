40d9b03d4484a2e34558ada56f476034fe1c76a5
4r7n <atj02005@gmail.com>
2026-04-13 17:07:42 +0100

    40

--- p40.py
+++ p40.py
@@ -1,1 +1,1 @@
-print((lambda x: ([l:=1] + [l:= l * int(x[10**i - 1]) for i in range(0, 7)])[-1])("".join([str(i) for i in range(1, 200000)])))
+print((lambda x: ([l:=1] + [l := l * int(x[10**i - 1]) for i in range(0, 7)])[-1])("".join([str(i) for i in range(1, 200000)])))
