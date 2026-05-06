093271f93788698082b871d155d3e4f99dca27f8
4r7n <atj02005@gmail.com>
2026-05-06 01:48:44 +0100

    99

--- p99.py
+++ p99.py
@@ -1,20 +1,17 @@
-import sys
-sys.set_int_max_str_digits(2122112212)
+from math import log
 
 with open("p99_base_exp.txt", "r") as f:
     rl = f.readlines()
 
-rl = [i.strip() for i in rl]
+rl = [tuple(map(int, i.strip().split(","))) for i in rl]
 m = 0
 L = 0
 
 for l, pwr in enumerate(rl):
-    n = eval("pow("+pwr+")")
+    n = pwr[1]*log(pwr[0])
+
     if n>m:
         m = n
         L = l
-
-    #if l%10==0:
-     #   print(l)
 
 print(L+1)
