c74d7830adeddfdb2d64c3b71fe41ba079dad89a
4r7n <atj02005@gmail.com>
2026-05-12 05:04:25 +0100

    79

--- p79.py
+++ p79.py
@@ -1,8 +1,8 @@
 attempts = [rl.strip("\n") for rl in open("p79_keylog.txt", "r")]
 #read in the file
 
-disticnt = set("".join(attempts))
-D = dict(zip(disticnt, [set() for l in range(len(disticnt))]))
+distinct = set("".join(attempts))
+D = dict(zip(distinct, [set() for l in range(len(distinct))]))
 #find all the distinct digits, this will be length of the password
 #we also assign each variable a set
 
@@ -15,7 +15,7 @@ for A in attempts:
         #for each value in the attempt, we track which values appear are greater than itself
         #this means that, by the end each value will have a distinct number of values greater than itself
 
-P = ["hi" for l in range(len(disticnt))]
+P = ["hi" for l in range(len(distinct))]
 
 for k in D.keys():
     P[len(D[k])] = k
