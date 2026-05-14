7447860dc858a629ed624ebd0fe871e797d47c26
4r7n <atj02005@gmail.com>
2026-05-14 16:58:43 +0100

    59

---
+++ p59.py
@@ -0,0 +1,28 @@
+F = list(map(int, open("p59_cipher.txt").read().split(",")))
+
+k = [97, 97, 97]
+
+def decrypt(F, k):
+    return [c ^ k[i % 3] for i, c in enumerate(F)]
+
+while True:
+    D = decrypt(F, k)
+
+    if all(32 <= c < 127 for c in D):
+        text = "".join(map(chr, D))
+
+        if " the " in text:
+            print(text)
+
+            print("\n", sum(D))
+            break
+
+    k[2] += 1
+
+    if k[2] == 123:
+        k[2] = 97
+        k[1] += 1
+
+    if k[1] == 123:
+        k[1] = 97
+        k[0] += 1
